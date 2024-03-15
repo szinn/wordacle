6c45191dc59a280e181e1ba84c2702a3bcc7b141
Scotte Zinn <scotte@zinn.ca>
2024-03-15 19:38:34 -0400

    feat: Added navigation bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
+<svelte:head>
+  <title>Wordacle</title>
+  <meta name="description" content="A Wordle Oracle" />
+</svelte:head>
+
 <h1>Welcome to Wordacle</h1>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>About Wordacle</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div>
+  <p>This is all about Wordacle.</p>
+</div>
